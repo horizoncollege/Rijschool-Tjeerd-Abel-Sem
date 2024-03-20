7308372896b96eed24b75faabf6338bf987e11d8
Robert Klapper <halo@pythaxprivate.net>
2024-03-20 10:25:43 -0400

    linux build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Linux build script voor project
+# Door Sem Vriesema
+
+sudo docker-compose up --detach
+sudo docker-compose run --rm npm install
+sudo docker-compose run --rm composer install
+sudo docker-compose run --rm composer require laravel/framework
+sudo docker-compose run --rm php php artisan key:generate
---
+++ src/resources/js/views/Login.vue
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+<template>
+    <div style="display: flex;justify-content: center;flex-direction: row;align-items: center;" class="center-div container">
+        <div class="container">
+            <div class="main_inner">
+                <div class="modal">
+                    <div class="form">
+                        <h1 style="color: black;">why not show dolboeb</h1>
+                        <input><br>
+                        <input>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.center-div {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    align-items: center;
+}
+
+.main_inner {}
+
+.main_inner.modal {}
+
+.main_inner.modal.form {}
+</style>
