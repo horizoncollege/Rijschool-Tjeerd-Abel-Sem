850d1b6d1958b296c8fe8c742de5fa1a19a7c8f1
AbelR <abelriezebos2006@gmail.com>
2024-04-03 13:57:00 +0200

    Commit #29 user-roles
    - changed user_role_pivot_table
    - changed Permission.php
    - changed User.php
    - added roles_table
    - added roles_permission_pivot_table
    - added Role.php
    - added RolePermissionSeeder.php
    - added RoleSeeder.php

--- src/app/Models/Permission.php
+++ src/app/Models/Permission.php
@@ -9,8 +9,8 @@ class Permission extends Model
 {
     use HasFactory;
 
-    public function users()
+    public function role()
     {
-        return $this->belongsToMany(User::class, 'user_permissions', 'user_id', 'permission_id');
+        return $this->belongsToMany(Role::class, 'roles_permission', 'role_id', 'permission_id');
     }
 }
---
+++ src/app/Models/Role.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+
+    public function permission()
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission', 'permission_id', 'role_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class, 'user_role', 'user_id', 'role_id');
+    }
+}
