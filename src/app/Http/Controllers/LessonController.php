e9c49401f90469fac442c9419ce22cdcaa12b90f
AbelR <abelriezebos2006@gmail.com>
2024-04-23 15:11:01 +0200

    Fully completed lesson backend

---
+++ src/app/Http/Controllers/LessonController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LessonDestroyRequest;
+use App\Http\Requests\LessonEditRequest;
+use App\Http\Requests\LessonIndexRequest;
+use App\Http\Requests\LessonShowRequest;
+use App\Http\Requests\LessonStoreRequest;
+use App\Http\Requests\LessonUpdateRequest;
+use App\Models\Lesson;
+use Illuminate\Support\Facades\Auth;
+
+class LessonController extends Controller
+{
+    public function show(LessonShowRequest $request, $id)
+    {
+        $lesson = Lesson::find($id);
+
+        return response()->json($lesson);
+    }
+
+    public function store(LessonStoreRequest $request)
+    {
+        $data = $request->all();
+
+        $lesson = new Lesson();
+        $lesson->goal = $data['goal'];
+        $lesson->status = $data['status'];
+        $lesson->start_date = $data['start_date'];
+        $lesson->end_date = $data['end_date'];
+        $lesson->day_of_month = $data['day_of_month'];
+        $lesson->teacher_id = Auth::user()->id;
+        $lesson->address = $data['address'];
+        $save = $lesson->save();
+
+        if ($save) {
+            return response()->json(['message' => 'Lesson created successfully', 200]);
+        } else {
+            return response()->json(['error' => 'Lesson not created', 500]);
+        }
+    }
+
+    public function destroy(LessonDestroyRequest $request, $id)
+    {
+        $lesson = Lesson::find($id);
+
+        $delete = $lesson->delete();
+
+        if ($delete) {
+            return response()->json(['message' => 'Lesson deleted successfully', 200]);
+        } else {
+            return response()->json(['error' => 'Lesson not deleted', 500]);
+        }
+    }
+
+
+    public function update(LessonUpdateRequest $request, $id)
+    {
+        $data = $request->all();
+
+        $lesson = Lesson::find($id);
+        $lesson->goal = $data['goal'];
+        $lesson->status = $data['status'];
+        $lesson->start_date = $data['start_date'];
+        $lesson->end_date = $data['end_date'];
+        $lesson->day_of_month = $data['day_of_month'];
+        $lesson->teacher_id = Auth::user()->id;
+        $lesson->address = $data['address'];
+        $update = $lesson->save();
+
+        if ($update) {
+            return response()->json(['message' => 'Lesson updated successfully', 200]);
+        } else {
+            return response()->json(['error' => 'Lesson not updated', 500]);
+        }
+    }
+
+    public function edit(LessonEditRequest $request, $id)
+    {
+        $lesson = Lesson::find($id);
+
+        return response()->json($lesson);
+    }
+
+    public function index(LessonIndexRequest $request)
+    {
+        $user = Auth::user();
+
+        $lessons = $user->lessons()->get();
+
+        return response()->json($lessons);
+    }
+
+}
---
+++ src/app/Http/Requests/LessonStoreRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class LessonStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $user = Auth::user();
+
+        foreach ($user->roles as $role) {
+            foreach ($role->permissions as $permission) {
+                if ($permission->name == 'can_lesson_store') {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'goal' => ['required', 'string', 'max:255'],
+            'status' => ['required', 'string', 'max:255'],
+            'start_date' => ['required', 'date'],
+            'end_date' => ['required', 'date'],
+            'day_of_month' => ['required', 'date'],
+            'address' => ['required', 'string', 'max:255'],
+        ];
+    }
+}
