40180702ec21daefb71eed97bb966d5e9b80f606
Abel R <abelriezebos2006@gmail.com>
2024-05-27 11:31:45 +0200

    Cleanup wrong user_role relation connection

--- src/app/Models/Role.php
+++ src/app/Models/Role.php
@@ -17,6 +17,6 @@ class Role extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class, 'user_role', 'user_id', 'role_id');
+        return $this->belongsToMany(User::class, 'user_role', 'role_id', 'user_id');
     }
 }
