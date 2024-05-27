1c9ba7e64692cc015bdb0cf22815f9ac058cf803
Abel R <abelriezebos2006@gmail.com>
2024-05-27 11:23:29 +0200

    maak students

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
@@ -15,18 +17,26 @@ class UserSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        for ($i = 0; $i < 10; $i++) {
-            DB::table('users')->insert([
-                'name' => $faker->name,
-                'email' => $faker->email,
-                'password' => $faker->password,
-                'phone' => $faker->phoneNumber(),
-                'birthday' => $faker->dateTimeThisCentury,
-                'address' => $faker->address(),
-                'second_address' => $faker->address(),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
+//        for ($i = 0; $i < 10; $i++) {
+//            DB::table('users')->insert([
+//                'name' => $faker->name,
+//                'email' => $faker->email,
+//                'password' => $faker->password,
+//                'phone' => $faker->phoneNumber(),
+//                'birthday' => $faker->dateTimeThisCentury,
+//                'address' => $faker->address(),
+//                'second_address' => $faker->address(),
+//                'created_at' => now(),
+//                'updated_at' => now(),
+//            ]);
+//        }
+
+        $role = Role::where('role', 'leerling')->first();
+
+        $user = User::factory()->count(5)->create();
+
+        $user->each(function ($user, $key) use ($role){
+            $user->roles()->attach($role['id']);
+        });
     }
 }
