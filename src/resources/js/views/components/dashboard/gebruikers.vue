b8e219f8c351325b22423bfbb697ca56a74f57ff
Tjeerd Wokke <171419@student.horizoncollege.nl>
2024-05-28 12:26:15 +0200

    update gebruikers.vue

--- src/resources/js/views/components/dashboard/gebruikers.vue
+++ src/resources/js/views/components/dashboard/gebruikers.vue
@@ -17,7 +17,7 @@ export default {
       })
       .catch((err) => console.error(err));
   },
-  
+
 };
 </script>
 
@@ -25,7 +25,38 @@ export default {
   <div class="content-row">
     <div class="content-blok">
       <h1>Studenten</h1>
-      <div class="multicolumn-blok">
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Naam</th>
+          <th>E-mail</th>
+          <th>Telefoonnummer</th>
+          <th>Verjaardag</th>
+          <th>Adres</th>
+          <th>2de adres</th>
+          <th>Manage</th>
+        </tr>
+
+        <tr v-for="student in students" :key="student.id">
+          <td>{{ student.id }}</td>
+          <td>{{ student.name }}</td>
+          <td>{{ student.email }}</td>
+          <td>{{ student.phone }}</td>
+          <td>{{ student.birthday }}</td>
+          <td> {{ student.address }}</td>
+          <td>{{ student.second_address }}</td>
+          <td>
+            <!-- <div class="multicolumn-knop knop-container">
+              <router-link class="knop knop-default" :to="{ path: '/pakket-details/' + pakket.id }">
+                <span class="knop-tekst">Bekijken</span>
+              </router-link>
+            </div> -->
+          </td>
+        </tr>
+      </table>
+
+
+      <!-- <div class="multicolumn-blok">
         <div class="multicolumn-rij" v-for="student in students" :key="student.id">
           <div class="user-details">
             <h2>{{ student.name }}</h2>
@@ -35,17 +66,17 @@ export default {
             <p><strong>Adres</strong> {{ student.address }}</p>
             <p><strong>2de adres</strong> {{ student.second_address }}</p>
 
-            <!-- <div class="multicolumn-knop knop-container">
+             <div class="multicolumn-knop knop-container">
             <router-link
               class="knop knop-default"
               :to="{ path: '/pakket-details/' + pakket.id }"
             >
               <span class="knop-tekst">Bekijken</span>
             </router-link>
-        </div> -->
+        </div> 
           </div>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
